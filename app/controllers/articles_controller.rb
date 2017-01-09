66d5b9f9f0550c438740ddfd7b67a76c540446d4
DejMar <marjanovic.dejan@gmail.com>
2017-01-09 16:59:18 +0100

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,65 @@
+class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :destroy, :show] # samo ove 4 etode pozivaju metodu set_article
+
+	# Metoda za ispis svih artikala
+	def index
+		@articles = Article.all
+	end
+
+	# Nova metoda koja kreira objekt Article
+	def new
+		@article = Article.new
+	end
+
+	# Metoda za mjenjanje podataka u article
+	def edit
+		
+	end
+
+	def create
+		# Spasavanje novog clanka	
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Article was successfully created"
+			# Nakon spasavanja se prebaci na show metodu, odnosno prikazuje unesene podatke
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	# Metoda za mjenjanje podataka
+	def update
+		
+		if @article.update(article_params)
+			flash[:success] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else	
+			render 'edit'
+		end
+	end
+
+	# Metoda za prikazivanje podataka 
+	def show
+		
+	end
+
+	# Metoda za brisanje podataka
+	def destroy		
+		
+		@article.destroy
+		flash[:danger] = "Article succefully deleted"
+		redirect_to articles_path
+
+	end
+
+private
+	# U ovoj metodi se ispituje da li su zadovoljeni svi uslovi kako bi se poslalo u metodu create
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+	#metoda koja prikazuje stranicu home
+	def home
+		
+	end
+
+	#metoda koja prikazuje stranicu about
+	def about
+		
+	end
+end
